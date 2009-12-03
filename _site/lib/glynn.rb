6e2b47efcb26d5f4d0c6f2c3e0d779e24e2e23ea
dmathieu <42@dmathieu.com>
2009-12-03 15:55:13 +0100

    gitignore

--- _site/lib/glynn.rb
+++
@@ -1,5 +0,0 @@
-$:.unshift File.dirname(__FILE__)     # For use/testing when no gem is installed
-require 'rubygems'
-
-require 'glynn/jekyll'
-
--- _site/spec/lib/jekyll.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
- 
-describe "Relation between glynn and jekyll" do
-  
-  it 'should create a new website' do
-    jekyll = Glynn::Jekyll.new
-    jekyll.build
-  end
-end
