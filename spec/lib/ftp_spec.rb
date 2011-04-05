b9fef67b3eeb426dcc4a62eac238cf6325fcfe7e
dmathieu <42@dmathieu.com>
2011-04-05 15:21:42 +0200

    appropriately mocking the ftp calls

--- spec/lib/ftp_spec.rb
+++ spec/lib/ftp_spec.rb
@@ -12,14 +12,19 @@ describe "FTP Interface" do
   end
   
   it 'should login to ftp server' do
+    @mock.should_receive(:close)
     Net::FTP.should_receive(:new).with('localhost', nil, nil).and_return(@mock)
+    
+    
     Glynn::Ftp.new('localhost').send(:connect) do |ftp|
       ftp.should eql(@mock)
     end
   end
   
   it 'should accept a username and password' do
+    @mock.should_receive(:close)
     Net::FTP.should_receive(:new).with('localhost', 'username', 'password').and_return(@mock)
+    
     Glynn::Ftp.new('localhost', 21, {:username => 'username', :password => 'password'}).send(:connect) do |ftp|
       ftp.should eql(@mock)
     end
@@ -29,10 +34,13 @@ describe "FTP Interface" do
     # We expect NET/FTP to create every file
     @mock.should_receive(:putbinaryfile).with('/test/README', '/blah/README').and_return(true)
     @mock.should_receive(:putbinaryfile).with('/test/subdir/README', '/blah/subdir/README').and_return(true)
+    @mock.should_receive(:mkdir).with('/blah')
+    @mock.should_receive(:mkdir).with('/blah/subdir').twice
+    
     FakeFS do
       # We create the fake files and directories
-      Dir.mkdir('/test')
-      Dir.mkdir('/test/subdir')
+      Dir.mkdir('/test') unless File.exists?('/test')
+      Dir.mkdir('/test/subdir') unless File.exists?('/test')
       File.open('/test/README', 'w') { |f| f.write 'N/A' }
       File.open('/test/subdir/README', 'w') { |f| f.write 'N/A' }
       
@@ -42,6 +50,8 @@ describe "FTP Interface" do
   end
   
   it 'should connect itself to the server and send the local file to distant directory' do
+    @mock.should_receive(:close)
+    
     FakeFS do
       interface = Glynn::Ftp.new('localhost')
       Net::FTP.should_receive(:new).with('localhost', nil, nil).and_return(@mock)
