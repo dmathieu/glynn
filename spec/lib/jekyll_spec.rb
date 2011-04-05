f8de2f3a3338452850a63edde5ac56ef0726d883
dmathieu <42@dmathieu.com>
2011-04-05 15:22:02 +0200

    that's pending for a fakefs patch

--- spec/lib/jekyll_spec.rb
+++ spec/lib/jekyll_spec.rb
@@ -10,6 +10,8 @@ describe "Relation between glynn and jekyll" do
   end
   
   it 'should create a new website' do
+    pending 'failing because of missing second parameter in Dir.glob of FakeFS.'
+    
     jekyll = Glynn::Jekyll.new({ 'source' => '/' })
     jekyll.build
   end
