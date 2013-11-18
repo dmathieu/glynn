5382746a76bdd06169ccf190f41124531cafa42d
Klaus Fl <kaeff@gmx.net>
2013-11-18 16:49:47 +0100

    Inlines Glynn::File#force_utf8

--- lib/glynn/file.rb
+++ lib/glynn/file.rb
@@ -1,17 +1,13 @@
 module Glynn
   class File
 
-    class << self
-      def is_bin?(f)
-        force_utf8(%x(file #{f})) !~ /text/
-      end
+    def self.is_bin?(f)
+      file_test = %x(file #{f})
 
-      private
+      # http://stackoverflow.com/a/8873922
+      file_test = file_test.encode('UTF-16', 'UTF-8', :invalid => :replace, :replace => '').encode('UTF-8', 'UTF-16')
 
-      def force_utf8(contents)
-        # http://stackoverflow.com/a/8873922
-        contents.encode('UTF-16', 'UTF-8', :invalid => :replace, :replace => '').encode('UTF-8', 'UTF-16')
-      end
+      file_test !~ /text/
     end
   end
 end
