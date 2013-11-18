10c0e29b576181d09c7c2af3bdff43c13c08a7c0
Klaus Fl <kaeff@gmx.net>
2013-11-18 13:49:53 +0100

    Double encodes shell returns to harden against arbitrary byte sequences

--- lib/glynn/file.rb
+++ lib/glynn/file.rb
@@ -1,8 +1,26 @@
+require 'iconv' unless String.method_defined?(:encode)
+
 module Glynn
   class File
 
-    def self.is_bin?(f)
-      %x(file #{f}) !~ /text/
+    class << self
+      def is_bin?(f)
+        force_utf8(%x(file #{f})) !~ /text/
+      end
+
+      private
+
+      def force_utf8(contents)
+        # http://stackoverflow.com/a/8873922
+        if String.method_defined?(:encode)
+          contents.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
+          contents.encode!('UTF-8', 'UTF-16')
+          contents
+        else
+          ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+          ic.iconv(file_contents)
+        end
+      end
     end
   end
 end
--- spec/lib/file_spec.rb
+++ spec/lib/file_spec.rb
@@ -1,21 +1,42 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "File module" do
-  before(:all)  do
-    FakeFS.activate!
-    FileUtils.mkdir("/test") if !File.directory?('/test')
-  end
-  after(:all) do
-    FakeFS.deactivate!
+  describe "with FakeFS" do
+    before(:all)  do
+      FakeFS.activate!
+      FileUtils.mkdir("/test") if !File.directory?('/test')
+    end
+    after(:all) do
+      FakeFS.deactivate!
+    end
+
+    it "should return file is text" do
+      File.open('/test/README', 'w') { |f| f.write 'N/A' }
+      Glynn::File.is_bin?('/test/README').should eql(true)
+    end
+
+    it "should return file is binary" do
+      File.open('/test/README', 'w') { |f| f.write ['0', '1', '2', '3'].pack('A3A3A3') }
+      Glynn::File.is_bin?('/test/README').should eql(true)
+    end
   end
 
-  it "should return file is text" do
-    File.open('/test/README', 'w') { |f| f.write 'N/A' }
-    Glynn::File.is_bin?('/test/README').should eql(true)
-  end
+  describe "shell integration" do
+    require 'tempfile'
 
-  it "should return file is binary" do
-    File.open('/test/README', 'w') { |f| f.write ['0', '1', '2', '3'].pack('A3A3A3') }
-    Glynn::File.is_bin?('/test/README').should eql(true)
+    let(:garbled_jpg) do
+      f = Tempfile.new('garbled.jpg')
+      f.write "\xff\xd8\xff\xe0\x00\x10\x4a\x46\x49\x46\x00\x01\x01\x01\x00\x48\x00\x48\x00\x00\xff\xfe\x00\x03\x2a\xff\xdb"
+      f.close
+      f
+    end
+
+    it "should handle binary content in file info" do
+      puts "Press any key to continue"
+      readline
+      Glynn::File.is_bin?(garbled_jpg.path).should be_true
+    end
+
   end
 end
+
